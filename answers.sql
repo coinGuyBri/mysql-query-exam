f8b9afcff57733b45cf18d5987a2370e8a0b9628
Brian Liotti <bdliotti@gmail.com>
2018-07-03 11:50:53 -0400

    draft complete

--- answers.sql
+++ answers.sql
@@ -62,21 +62,28 @@ SELECT p.firstName, p.lastName, sum(f.miles) miles FROM passenger p JOIN reserva
 11. Provide a list of American airline flights ordered by route and arrival date and time. Your results must look like this:
 */
 
-SELECT a.name Name, concat_ws(' --> ',f.departAirport, f.arriveAirport) Route, DATE(f.scheduledArriveDateTime) 'Arrive Date', TIME(f.scheduledArriveDateTime) 'Arrive Time' FROM flight f JOIN airline a ON a.ID = f.airlineID WHERE a.name = 'American' ORDER BY Route, 'Arrival Date', 'Arrival Time';
+SELECT a.name Name, concat_ws(' --> ',f.departAirport, f.arriveAirport) Route, DATE(f.scheduledArriveDateTime) `Arrive Date`, TIME(f.scheduledArriveDateTime) `Arrive Time` FROM flight f JOIN airline a ON a.ID = f.airlineID WHERE a.name = 'American' ORDER BY Route, `Arrive Date`, `Arrive Time`;
 
 /*
 12. Provide a report that counts the number of reservations and totals the reservation costs (as Revenue) by Airline, flight, and route. Order the report by total revenue in descending order.
 */
 
+SELECT a.name Name, f.flightNumber Flight, concat_ws(' --> ',f.departAirport, f.arriveAirport) Route, count(r.ID), sum(r.cost) Revenue FROM flight f JOIN airline a ON a.ID = f.airlineID JOIN reservation r ON r.flightID = f.ID GROUP BY f.flightNumber ORDER BY Revenue DESC;
+
 /*
 13. List the average cost per reservation by route. Round results down to the dollar.
 */
 
+SELECT concat_ws(' --> ',f.departAirport, f.arriveAirport) Route, floor(avg(r.cost)) `Avg Revenue` FROM flight f JOIN reservation r ON r.flightID = f.ID GROUP BY f.flightNumber ORDER BY `Avg Revenue` DESC;
 
 /*
 14. List the average miles per flight by airline.
 */
 
+SELECT a.name Airline, avg(f.miles) `Avg Miles Per Flight` FROM flight f JOIN airline a ON a.ID = f.airlineID GROUP BY Airline;
+
 /*
 15. Which airlines had flights that arrived early?
 */
+
+SELECT DISTINCT a.name Airline FROM flight f JOIN airline a ON a.ID = f.airlineID WHERE TIME(actualArriveDateTime) - TIME(scheduledArriveDateTime) <= 0;
