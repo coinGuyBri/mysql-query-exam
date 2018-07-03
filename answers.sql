d1f23c49b6be34b427d81f116ce18e04c3c0b7d6
Brian Liotti <bdliotti@gmail.com>
2018-07-03 10:27:08 -0400

    5,6 and 7 complete

--- answers.sql
+++ answers.sql
@@ -26,13 +26,19 @@ SELECT DISTINCT a.name Airline FROM flight f JOIN airline a ON a.ID = f.airlineI
 5. Provide a list of airlines, flight numbers, departing airports, and arrival airports where flights departed on time.
 */
 
+SELECT a.name, f.flightNumber, f.departAirport, f.arriveAirport FROM flight f RIGHT JOIN airline a ON a.ID = f.airlineID WHERE TIME(actualDepartDateTime) - TIME(scheduledDepartDateTime) = 0;
+
 /*
 6. Provide a list of airlines, flight numbers, gates, status, and arrival _times_ arriving into Charlotte (CLT) on 10-30-2017. Order your results by the arrival time.
 */
 
+SELECT a.name Airline, f.flightNumber Flight, f.gate Gate, TIME(f.scheduledArriveDateTime) Arrival, f.status Status FROM flight f RIGHT JOIN airline a ON a.ID = f.airlineID WHERE f.arriveAirport = 'CLT' AND DATE(f.scheduledArriveDateTime) = '2017-10-30' ORDER BY f.scheduledArriveDateTime;
+
 /*
 7. List the number of reservations by flight number. Order by reservations in descending order.
 */
+
+SELECT f.flightNumber flight, count(r.ID) reservations FROM reservation r JOIN flight f ON r.flightID = f.ID GROUP BY f.flightNumber ORDER BY reservations DESC;
 
 /*
 8. List the average ticket cost for coach by airline and route. Order by AverageCost in descending order.
